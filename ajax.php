2a9cc46ff4a93bf831ed8fdd67fa255b2e3ce72f
ThikerXCX <rifkimuhammad967@gmail.com>
2023-07-10 14:54:16 +0700

    test

--- ajax.php
+++ ajax.php
@@ -4,3 +4,4 @@ if ($mod == 'simpan_aturan') {
     $db->query("UPDATE tb_aturan SET kode_himpunan='$_POST[kode_himpunan]' WHERE kode_kriteria='$_POST[kode_kriteria]' AND no_aturan='$_POST[no_aturan]'");
     echo "Simpan Aturan Berhasil!";
 }
+//testing
